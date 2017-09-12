5c109bb97e0b5c168b343f870a56166af303254e
BenjaminChenTW <s910190benjamin@gmail.com>
2017-09-13 01:53:06 +0800

    change log, should use compression only on HTML, start redis, icon url incomplete

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var redis = require('redis');
 var passport = require('passport');
 var compression = require('compression');
+var ua = require('universal-analytics');
 var debug = require('debug')('goodtogo_backend:app');
 var debugError = require('debug')('goodtogo_backend:appERR');
-var redis = require('redis');
 
 var index = require('./routes/index');
 var loggingDefault = require('./routes/loggingDefault');
@@ -22,16 +23,16 @@ var config = require('./config/config');
 var app = express();
 
 // GA
-var ua = require('universal-analytics');
-var visitor = ua(config.GA_TRACKING_ID, {https: true});
+var visitor = ua(config.GA_TRACKING_ID, { https: true });
+
 function GAtrigger(req, res, next) {
-	visitor.set('ua', req.headers['user-agent']);
-	visitor.pageview(req.url, function (err) {
-		if (err !== null){
-      console.log('Failed to trigger GA: ' + err);
-		}
-	});
-	next();
+    visitor.set('ua', req.headers['user-agent']);
+    visitor.pageview(req.url, function(err) {
+        if (err !== null) {
+            console.log('Failed to trigger GA: ' + err);
+        }
+    });
+    next();
 }
 
 // view engine setup
@@ -40,31 +41,29 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(compression());
-app.use(logger('short'));
+// app.use(compression());
+app.use(logger(':remote-addr :remote-user :method :url HTTP/:http-version :status - :response-time ms - :date'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(GAtrigger); // Trigger Google Analytics
 app.use(passport.initialize());
 app.set('passport', passport);
 
 debug.log = console.log.bind(console);
 mongoose.Promise = global.Promise;
-mongoose.connect(config.dbUrl,config.dbOptions, function(err){
-  if (err) next(err);
-  debug('mongoDB connect succeed');
+mongoose.connect(config.dbUrl, config.dbOptions, function(err) {
+    if (err) next(err);
+    debug('mongoDB connect succeed');
 });
 require('./models/userQuery'); // pass passport for configuration
 
-// redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS)
-/*
-redis.createClient(6379, config.reditUrl, {}).on('ready',function(err){
-  if (err) next(err);
-  debug('redisDB create succeed');
+redis.createClient(6379, config.redisUrl, {}).on('ready', function(err) {
+    if (err) next(err);
+    debug('redisDB create succeed');
 });
-*/
+
 app.all('/*', loggingDefault);
 
 // app.use('/', index);
@@ -75,25 +74,25 @@ app.use('/images', images);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  if (typeof err.status === 'undefined'){
-    debugError(err.message);
-    res.status(500);
-    res.json({type: 'globalError', message: 'Unexpect Error. Please contact network administrator with following data: ' + JSON.stringify(req.headers)});
-  } else {
-    res.status(err.status);
-    res.json({type: 'globalError', message: err.message});
-  }
+    if (typeof err.status === 'undefined') {
+        debugError(err.message);
+        res.status(500);
+        res.json({ type: 'globalError', message: 'Unexpect Error. Please contact network administrator with following data: ' + JSON.stringify(req.headers) });
+    } else {
+        res.status(err.status);
+        res.json({ type: 'globalError', message: err.message });
+    }
 });
 
 module.exports = app;
