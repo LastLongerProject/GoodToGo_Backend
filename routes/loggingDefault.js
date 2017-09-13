81a5e89595659c102e79050ec39c0bc1987cdc86
BenjaminChenTW <s910190benjamin@gmail.com>
2017-09-13 13:10:41 +0800

    fix logging auth

--- routes/loggingDefault.js
+++ routes/loggingDefault.js
@@ -3,45 +3,45 @@ var router = express.Router();
 var logging = require('../models/loggingQuery').withoutAuth;
 var logRed = require('../models/loggingQuery').redirect;
 
-var passLogging = false;
-
+var passLoggingWithoutAuth = true;
+/*
 router.all('/images/*', function(req, res, next) {
-    passLogging = true;
+    passLoggingWithoutAuth = true;
     next();
-});
-
+});*/
+/*
 router.all('/containers/:id', function(req, res, next) {
-    passLogging = true;
+    passLoggingWithoutAuth = true;
     logRed(req, res, function(err) {
         if (err) return next(err);
         next();
     });
-});
+});*/
 
 router.all('/users/signup', function(req, res, next) {
-    passLogging = true;
+    passLoggingWithoutAuth = false;
     next();
 });
 
 router.all('/users/login', function(req, res, next) {
-    passLogging = true;
+    passLoggingWithoutAuth = false;
     next();
 });
 
 router.all('/stores/list', function(req, res, next) {
-    passLogging = true;
+    passLoggingWithoutAuth = false;
     next();
 });
 
 router.all('/*', function(req, res, next) {
-    if (!passLogging) {
-        passLogging = false;
+    if (!passLoggingWithoutAuth) {
+        passLoggingWithoutAuth = true;
         logging(req, res, function(err) {
             if (err) return next(err);
             next();
         });
     } else {
-        passLogging = false;
+        // passLoggingWithoutAuth = false;
         next();
     }
 });
