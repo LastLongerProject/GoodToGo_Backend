8f6dddc15b9a6099dbfa60e488c007e2949ad04b
Benjamin Chen <s910190benjamin@gmail.com>
2018-07-18 02:32:13 +0800

    please

--- routes/stores.js
+++ routes/stores.js
@@ -709,7 +709,7 @@ router.get('/history/byContainerType', regAsStore, validateRequest, function(req
                     dateCtr = 0;
                     checkpoint = dateCheckpoint(dateCtr);
                     for (var i = 0; i < returnTrades.length; i++) {
-                        if ((checkpoint - returnTrades[i].tradeTime) > 1000 * 60 * 60 * 24) {
+                        if (parseInt(checkpoint - returnTrades[i].tradeTime) > 1000 * 60 * 60 * 24) {
                             dateCtr--;
                             checkpoint = dateCheckpoint(dateCtr);
                             console.log(fullDateString(checkpoint), checkpoint, returnTrades[i].tradeTime, checkpoint - returnTrades[i].tradeTime, checkpoint - returnTrades[i].tradeTime > 1000 * 60 * 60 * 24);
