da6994fb1fb0790a1696f56117797ae897da3c2e
Benjamin Chen <s910190benjamin@gmail.com>
2018-08-17 21:01:12 +0800

    finish index

--- routes/manage.js
+++ routes/manage.js
@@ -13,9 +13,117 @@ var dateCheckpoint = require('../models/toolKit').dateCheckpoint;
 
 var Store = require('../models/DB/storeDB');
 var Trade = require('../models/DB/tradeDB');
+var User = require('../models/DB/userDB');
 
 const MILLISECONDS_OF_A_WEEK = 1000 * 60 * 60 * 24 * 7;
 const MILLISECONDS_OF_A_DAY = 1000 * 60 * 60 * 24;
+const MILLISECONDS_OF_LOST_CONTAINER = MILLISECONDS_OF_A_DAY * 3;
+
+router.get('/index', regAsAdminManager, validateRequest, function (req, res, next) {
+    var result = {
+        summary: {
+            userAmount: 0,
+            storeAmount: 0,
+            activityAmount: 0
+        },
+        activityHistorySummary: {
+            usedAmount: 0,
+            lostAmount: 0,
+            totalDuration: 0
+        },
+        shopHistorySummary: {
+            usedAmount: 0,
+            lostAmount: 0,
+            totalDuration: 0,
+            quantityOfBorrowingFromDiffPlace: 0
+        },
+        shopRecentHistorySummary: {
+            usedAmount: 0,
+            lostAmount: 0,
+            totalDuration: 0,
+            quantityOfBorrowingFromDiffPlace: 0
+        }
+    };
+    User.count((err, userAmount) => {
+        if (err) return next(err);
+        result.summary.userAmount = userAmount;
+        Store.count({
+            active: true
+        }, (err, storeAmount) => {
+            if (err) return next(err);
+            result.summary.storeAmount = storeAmount;
+            var tradeQuery = {
+                'tradeType.action': {
+                    '$in': ['Sign', 'Rent', 'Return', 'UndoReturn', 'ReadyToClean', 'UndoReadyToClean']
+                }
+            };
+            if (false) // 讀快取
+                tradeQuery.tradeTime = {
+                    '$gte': dateCheckpoint(0)
+                };
+            Trade.find(tradeQuery, function (err, tradeList) {
+                if (err) return next(err);
+                cleanUndo(['Return', 'ReadyToClean'], tradeList);
+
+                var now = Date.now();
+                var lastUsed = {};
+                var rentedContainer = {};
+                var signedContainer = {};
+                var usedTime = [];
+                var usedTime_recent = [];
+                tradeList.forEach(function (aTrade) {
+                    var containerKey = aTrade.container.id + "-" + aTrade.container.cycleCtr;
+                    lastUsed[aTrade.container.id] = {
+                        time: aTrade.tradeTime,
+                        action: aTrade.tradeType.action
+                    };
+                    if (aTrade.tradeType.action === "Sign") {
+                        signedContainer[containerKey] = {
+                            time: aTrade.tradeTime,
+                            storeID: aTrade.newUser.storeID
+                        };
+                    } else if (aTrade.tradeType.action === "Rent") {
+                        rentedContainer[containerKey] = {
+                            time: aTrade.tradeTime
+                        };
+                    } else if (aTrade.tradeType.action === "Return") {
+                        var recent = now - aTrade.tradeTime <= MILLISECONDS_OF_A_WEEK;
+                        result.shopHistorySummary.usedAmount++;
+                        if (recent) {
+                            result.shopRecentHistorySummary.usedAmount++;
+                        }
+                        if (rentedContainer[containerKey]) {
+                            var duration = aTrade.tradeTime - rentedContainer[containerKey].time;
+                            usedTime.push(duration);
+                            if (recent) {
+                                usedTime_recent.push(duration);
+                            }
+                        }
+                        if (aTrade.newUser.storeID !== signedContainer[containerKey].storeID) {
+                            result.shopHistorySummary.quantityOfBorrowingFromDiffPlace++;
+                            if (recent) {
+                                result.shopRecentHistorySummary.quantityOfBorrowingFromDiffPlace++;
+                            }
+                        }
+                    }
+                });
+
+                for (var containerID in lastUsed) {
+                    if (lastUsed[containerID].action !== "ReadyToClean" && (now - lastUsed[containerID].time) >= MILLISECONDS_OF_LOST_CONTAINER) {
+                        // console.log(containerID, lastUsed[containerID])
+                        result.shopHistorySummary.lostAmount++;
+                    }
+                }
+
+                result.shopHistorySummary.totalDuration = usedTime.reduce((a, b) => (a + b));
+                result.shopRecentHistorySummary.totalDuration = usedTime_recent.reduce((a, b) => (a + b));
+
+                res.json(result);
+            });
+        });
+    });
+});
+
 
 router.get('/shop', regAsAdminManager, validateRequest, function (req, res, next) {
     Store.find({
@@ -49,24 +157,10 @@ router.get('/shop', regAsAdminManager, validateRequest, function (req, res, next
         }, function (err, tradeList) {
             if (err) return next(err);
 
+            cleanUndo('ReadyToClean', tradeList);
+
             var usedContainer = {};
             var unusedContainer = {};
-            var recordToRemove = [];
-            for (var i = tradeList.length - 1; i >= 0; i--) {
-                if (tradeList[i].tradeType.action === "UndoReadyToClean") {
-                    var containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr;
-                    recordToRemove.push(containerKey);
-                    tradeList.splice(i, 1);
-                } else if (tradeList[i].tradeType.action === "ReadyToClean") {
-                    var containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr;
-                    var removeIndex = recordToRemove.indexOf(containerKey);
-                    if (removeIndex !== -1) {
-                        recordToRemove.splice(recordToRemove, 1);
-                        tradeList.splice(i, 1);
-                    }
-                }
-            }
-
             tradeList.forEach(function (aTrade) {
                 var containerKey = aTrade.container.id + "-" + aTrade.container.cycleCtr;
                 if (aTrade.tradeType.action === "Sign") {
@@ -93,7 +187,6 @@ router.get('/shop', regAsAdminManager, validateRequest, function (req, res, next
             }
 
             var weeklyAmountByStore = {};
-
             var weekCheckpoint = getWeekCheckpoint(Object.entries(usedContainer)[0][1].time);
             var todayCheckpoint = dateCheckpoint(0);
             for (var usedContainerKey in usedContainer) {
@@ -190,24 +283,10 @@ router.get('/shopDetail/:id', regAsAdminManager, validateRequest, function (req,
         }, function (err, tradeList) {
             if (err) return next(err);
 
+            cleanUndo('ReadyToClean', tradeList);
+
             var usedContainer = {};
             var unusedContainer = {};
-            var recordToRemove = [];
-            for (var i = tradeList.length - 1; i >= 0; i--) {
-                if (tradeList[i].tradeType.action === "UndoReadyToClean") {
-                    var containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr;
-                    recordToRemove.push(containerKey);
-                    tradeList.splice(i, 1);
-                } else if (tradeList[i].tradeType.action === "ReadyToClean") {
-                    var containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr;
-                    var removeIndex = recordToRemove.indexOf(containerKey);
-                    if (removeIndex !== -1) {
-                        recordToRemove.splice(recordToRemove, 1);
-                        tradeList.splice(i, 1);
-                    }
-                }
-            }
-
             tradeList.forEach(function (aTrade) {
                 var containerKey = aTrade.container.id + "-" + aTrade.container.cycleCtr;
                 if (aTrade.tradeType.action === "Sign") {
@@ -229,7 +308,6 @@ router.get('/shopDetail/:id', regAsAdminManager, validateRequest, function (req,
             result.toUsedAmount = Object.keys(unusedContainer).length;
 
             var weeklyAmount = {};
-
             var weekCheckpoint = getWeekCheckpoint(Object.entries(usedContainer)[0][1].time);
             var todayCheckpoint = dateCheckpoint(0);
             var recentCheckpoint = dateCheckpoint(-6);
@@ -300,6 +378,46 @@ router.get('/shopDetail/:id', regAsAdminManager, validateRequest, function (req,
         });
     });
 });
+
+function cleanUndo(action, tradeList) {
+    var undoAction;
+    var containerKey;
+    var recordToRemove = [];
+    if (typeof action === String) {
+        undoAction = "Undo" + action;
+        for (var i = tradeList.length - 1; i >= 0; i--) {
+            if (tradeList[i].tradeType.action === undoAction) {
+                containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr;
+                recordToRemove.push(containerKey);
+                tradeList.splice(i, 1);
+            } else if (tradeList[i].tradeType.action === action) {
+                containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr;
+                var removeIndex = recordToRemove.indexOf(containerKey);
+                if (removeIndex !== -1) {
+                    recordToRemove.splice(recordToRemove, 1);
+                    tradeList.splice(i, 1);
+                }
+            }
+        }
+    } else if (Array.isArray(action)) {
+        undoAction = ["Undo" + action[0], "Undo" + action[1]];
+        for (var i = tradeList.length - 1; i >= 0; i--) {
+            if (tradeList[i].tradeType.action === undoAction[0] || tradeList[i].tradeType.action === undoAction[1]) {
+                containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr + "-" + tradeList[i].tradeType.action.slice(4);
+                recordToRemove.push(containerKey);
+                tradeList.splice(i, 1);
+            } else if (tradeList[i].tradeType.action === action[0] || tradeList[i].tradeType.action === action[1]) {
+                containerKey = tradeList[i].container.id + "-" + tradeList[i].container.cycleCtr + "-" + tradeList[i].tradeType.action;
+                var removeIndex = recordToRemove.indexOf(containerKey);
+                if (removeIndex !== -1) {
+                    recordToRemove.splice(recordToRemove, 1);
+                    tradeList.splice(i, 1);
+                }
+            }
+        }
+    }
+
+}
 
 function getWeekCheckpoint(date) {
     var timezoneFix = 0;
