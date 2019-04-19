26a8746f8c95b3362fc83d89d49ba03abc967ae7
BenjaminChenTW <s910190benjamin@gmail.com>
2019-04-20 03:10:03 +0800

    finish scheduler

--- helpers/notifications/enums/events.js
+++ helpers/notifications/enums/events.js
@@ -1,5 +1,9 @@
 module.exports = Object.freeze({
     CONTAINER_DELIVERY: "container_delivery",
     CONTAINER_RENT: "container_rent",
-    CONTAINER_RETURN: "container_return"
+    CONTAINER_RETURN: "container_return",
+    CONTAINER_RETURN_VIP: "container_return_vip",
+    USER_ALMOST_OVERDUE: "user_almost_overdue",
+    USER_BANNED: "user_banned",
+    USER_UNBANNED: "user_unbanned"
 });
--- helpers/scheduler.js
+++ helpers/scheduler.js
@@ -1,5 +1,13 @@
 const User = require('../models/DB/userDB');
 const UserKeys = require('../models/DB/userKeysDB');
+const Coupon = require('../models/DB/couponDB');
+const Container = require('../models/DB/containerDB');
+const DataCacheFactory = require('../models/dataCacheFactory');
+const PurchaseStatus = require('../models/enums/userEnum').PurchaseStatus;
+
+const NotificationCenter = require('../helpers/notifications/center');
+const NotificationEvent = require('../helpers/notifications/enums/events');
+
 const appInit = require('./appInit');
 const dateCheckpoint = require('@lastlongerproject/toolkit').dateCheckpoint;
 
@@ -25,9 +33,16 @@ module.exports = function () {
         if (!bot) return debug.error('missing bot acount');
         var dateNow = new Date();
         var shouldWait = dateCheckpoint(1) - dateNow;
-        setTimeout(function () {
+        setTimeout(function timeSensitiveTask() {
             setInterval(function tasks() {
-                debug.log('scheduler start');
+                debug.log('[Scheduler | Time-Sensitive] start');
+                checkCouponIsExpired();
+                checkUsersShouldBeBanned();
+            }(), 1000 * 60 * 60 * 24);
+        }, shouldWait);
+        setTimeout(function noneTimeSensitiveTask() {
+            setInterval(function tasks() {
+                debug.log('[Scheduler | None-Time-Sensitive] start');
                 setTimeout(appInit.refreshContainer, 0, bot, cb);
                 setTimeout(appInit.refreshStore, 1000 * 60 * 5, cb);
                 setTimeout(appInit.refreshActivity, 1000 * 60 * 7, cb);
@@ -63,3 +78,100 @@ module.exports = function () {
         }, shouldWait + 1000 * 60 * 60);
     });
 };
+
+const DueDays = {
+    free_user: 1,
+    purchased_user: 7
+};
+
+function checkCouponIsExpired() {
+    const CouponTypeDict = DataCacheFactory.get("couponType");
+    Coupon.find({
+        "expired": false
+    }, (err, couponList) => {
+        if (err) return debug.error(err);
+        const now = Date.now();
+        couponList.forEach(aCoupon => {
+            if (CouponTypeDict[aCoupon.couponType].expirationDate < now) {
+                aCoupon.expired = true;
+                aCoupon.save(err => {
+                    if (err) return debug.error(err);
+                });
+            }
+        });
+    });
+}
+
+function checkUsersShouldBeBanned() {
+    User.find({
+        "agreeTerms": true
+    }, (err, userList) => {
+        if (err) return debug.error(err);
+        const userDict = {};
+        const userPhoneList = userList.map(aUser => {
+            const userPhone = aUser.user.phone;
+            userDict[userPhone] = {
+                dbUser: aUser,
+                almostOverdue: [],
+                overdue: []
+            };
+            return userPhone;
+        });
+        Container.find({
+            "conbineTo": {
+                "$in": userPhoneList
+            },
+            "statusCode": 2
+        }, (err, containerList) => {
+            if (err) return debug.error(err);
+
+            const now = Date.now();
+            containerList.forEach(aContainer => {
+                const userPhone = aContainer.conbineTo;
+                const purchaseStatus = userDict[userPhone].dbUser.hasPurchase ?
+                    PurchaseStatus.PURCHASED_USER :
+                    PurchaseStatus.FREE_USER;
+                const daysToDue = computeDaysOfUsing(aContainer.lastUsedAt, now) - DueDays[purchaseStatus];
+                if (daysToDue <= 0) {
+                    userDict[userPhone].overdue.push(aContainer);
+                } else if (daysToDue <= 1) {
+                    userDict[userPhone].almostOverdue.push(aContainer);
+                }
+            });
+
+            for (let userPhone in userDict) {
+                const dbUser = userDict[userPhone].dbUser;
+                if (userDict[userPhone].overdue.length > 0)
+                    banUser(dbUser);
+                else if (userDict[userPhone].almostOverdue.length > 0)
+                    noticeUserWhoIsGoingToBeBanned(dbUser);
+                else if (userDict[userPhone].overdue.length === 0 && dbUser.hasBanned && dbUser.bannedTimes === 1)
+                    unbanUser(dbUser);
+            }
+        });
+    });
+}
+
+function banUser(dbUser) {
+    dbUser.hasBanned = true;
+    dbUser.save(err => {
+        if (err) return debug.error(err);
+    });
+    NotificationCenter.emit(NotificationEvent.USER_BANNED, dbUser, null);
+}
+
+function noticeUserWhoIsGoingToBeBanned(dbUser) {
+    NotificationCenter.emit(NotificationEvent.USER_ALMOST_OVERDUE, dbUser, null);
+}
+
+function unbanUser(dbUser) {
+    dbUser.hasBanned = false;
+    dbUser.save(err => {
+        if (err) return debug.error(err);
+    });
+    NotificationCenter.emit(NotificationEvent.USER_UNBANNED, dbUser, null);
+}
+
+function computeDaysOfUsing(dateToCompute, now) {
+
+}
