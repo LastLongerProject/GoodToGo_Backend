2d31c7d293ead5ef5c1e4ab804c26b85d059ed11
BenjaminChenTW <s910190benjamin@gmail.com>
2019-04-19 15:11:47 +0800

    add coution template

--- models/DB/couponTypeDB.js
+++ models/DB/couponTypeDB.js
@@ -42,11 +42,20 @@ const generalCoution = "\t•　本優惠券限使用一次，不得重複使用
     "\t•　確定使用優惠券後，須點選「我要使用」向店家出示容器借用畫面。\n" +
     "\t•　若遇不可抗拒之事由導致本優惠券無法使用，好盒器保留更換其他等值優惠券之權利。\n" +
     "\t•　好盒器保有相關細節最終解釋權，如有未盡事宜，好盒器得修訂之。";
+const templateKeyValidater = /[a-z_]+/;
+const extraCoutionTemplate = Object.freeze({
+    takeout_drink: "\t•　本優惠券限使用於外帶飲品，並須以好盒器循環容器盛裝。" +
+        "\t•　一杯飲品限使用一張優惠券，不得與其他折價券或優惠活動同時使用。"
+});
 
 userSchema.methods.generateCoution = function () {
+    let formattedExtraCoution = this.extraCoution !== null ? this.extraCoution + "\n" : "";
+    if (templateKeyValidater.test(formattedExtraCoution) && extraCoutionTemplate[formattedExtraCoution])
+        formattedExtraCoution = extraCoutionTemplate[formattedExtraCoution];
+
     return (this.extraContent !== null ? this.extraContent + "\n" : "") +
         coutionTitle +
-        (this.extraCoution !== null ? this.extraCoution + "\n" : "") +
+        formattedExtraCoution +
         generalCoution;
 };
 
