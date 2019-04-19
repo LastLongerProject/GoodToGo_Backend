b2f35001f9f0640cada86a687bdce96173cd1ea0
BenjaminChenTW <s910190benjamin@gmail.com>
2019-04-20 01:00:07 +0800

    fix apidoc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-19T16:43:48.791Z",
+    "time": "2019-04-19T16:59:49.420Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
--- routes/users.js
+++ routes/users.js
@@ -215,7 +215,8 @@ router.post(
  * @api {post} /users/signup/lineUser Sign up for new line user
  * @apiUse DefaultSecurityMethod
  * 
- * @apiParam {String} lineId lineId of the User.
+ * @apiParam {String} line_liff_userID line_liff_userID of the User.
+ * @apiParam {String} line_channel_userID line_channel_userID of the User.
  * @apiParam {String} phone phone of the User.
  * 
  * @apiSuccessExample {json} Success-Response:
