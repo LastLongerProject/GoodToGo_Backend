30ecc51dd9383639000eef13216290f5d3fb6579
BenjaminChen_TW <s910190benjamin@gmail.com>
2020-03-01 19:02:56 +0800

    update role api doc

--- routes/users/role.js
+++ routes/users/role.js
@@ -13,6 +13,7 @@ const User = require('../../models/DB/userDB');
  * @apiPermission customer
  * 
  * @api {get} /role/checkIsExisted/:roleType Check Is the Role Existed in My RoleList
+ * @apiParam {...} Use para to check if an para is in Role. eg. manager:Boolean
  * @apiUse JWT
  * 
  * @apiSuccessExample {json} Success-Response:
@@ -54,6 +55,7 @@ router.get('/checkIsExisted/:roleType', validateRequest, function (req, res, nex
  * @apiPermission admin_manager
  * 
  * @api {get} /role/checkIsExisted/:phone/:roleType Check Is the Role Existed in User's RoleList
+ * @apiParam {...} Use para to check if an para is in Role. eg. manager:Boolean
  * @apiUse JWT
  * 
  * @apiSuccessExample {json} Success-Response:
@@ -107,6 +109,13 @@ router.get('/checkIsExisted/:phone/:roleType', checkRoleIsAdmin(), validateReque
  * @apiPermission admin_manager
  * 
  * @api {put} /role/add/:phone Add a Role to a User's RoleList
+ * @apiParam {options} Provide needed para for Role creation as a Object. eg. manager:Boolean
+ * @apiParamExample {json} Request-Example:
+    {
+        options : {
+            manager: true
+        }
+    }
  * @apiUse JWT
  * 
  * @apiSuccessExample {json} Success-Response:
@@ -163,6 +172,14 @@ router.put('/add/:phone', checkRoleIsAdmin(), validateRequest, function (req, re
  * @apiPermission admin_manager
  * 
  * @api {put} /role/remove/:phone Delete a Role from a User's RoleList
+ * @apiParam {options} Provide needed para to find the Role for deletion. eg. manager:Boolean
+ * @apiParamExample {json} Request-Example:
+    {
+        options : {
+            manager: true,
+            stationID: 0
+        }
+    }
  * @apiUse JWT
  * 
  * @apiSuccessExample {json} Success-Response:
