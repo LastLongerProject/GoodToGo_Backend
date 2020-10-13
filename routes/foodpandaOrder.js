a00526bd74b5fc818ffde2331670363d17c66124
ezou <ezoushen@gmail.com>
2020-10-13 18:10:41 +0800

    - add routes

--- routes/foodpandaOrder.js
+++ routes/foodpandaOrder.js
@@ -15,7 +15,7 @@ const DataCacheFactory = require('../models/dataCacheFactory');
 const { generateUUID } = require('../helpers/tools');
 
 router.post('/', validateRequest, validateLine, validateStoreCode, (req, res, next) => {
-    const {phone, userOrders} = req.body.phone;
+    const {phone, userOrders} = req.body;
     
     User.findOne({ "user.phone": phone })
         .then((user) => {
@@ -59,3 +59,61 @@ router.post('/', validateRequest, validateLine, validateStoreCode, (req, res, ne
             })
         })
 })
+
+router.patch('/', validateRequest, validateLine, validateStoreCode, (res, req, next) => {
+    const { phone, userOrders, order } = req.body;
+
+    User.findOne({ "user.phone": phone })
+        .then((user) => {
+            UserOrder.find({
+                "orderID": { $in: userOrders },
+                "user": user._id
+            })
+                .exec()
+                .then(orders => {
+                    if (orders.length !== userOrders.length) {
+                        return res.status(403).json({
+                            code: 'L021',
+                            type: 'validatingUserOrder',
+                            message: 'User Order not found'
+                        })
+                    }
+
+                    return FoodpandaOrder
+                        .findOne({ orderID: order })
+                        .then((foodpandaOrder) => {
+                            foodpandaOrder.userOrders = orders
+                        })
+                        .catch(_ => {
+                            return res.status(403).json({
+                                code: 'L022',
+                                type: 'validatingFoodpandaOrder',
+                                message: 'Foodpanda Order not found'
+                            })
+                        })
+                })
+                .then( _ => {
+                    return res.status(200).json()
+                })
+                .catch((_) => {
+                    return res.status(403).json({
+                        code: 'L021',
+                        type: 'validatingUserOrder',
+                        message: 'User Order not found'
+                    })
+                })
+        })
+        .catch((_) => {
+            return res.status(401).json({
+                code: 'B002',
+                type: 'validatingUser',
+                message: 'User not Found'
+            })
+        })
+})
+
+router.put('/archive', validateRequest, validateLine, (req, res, next) => {
+
+})
+
+module.exports = router;
